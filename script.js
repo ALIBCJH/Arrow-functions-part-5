f3dbaf209f905c433c672ff953c2592d15f43d02
simonjuma_230 <simon.ombom22@students.dkut.ac.ke>
2024-06-06 00:04:48 +0300

    Merge test_branch into master

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ console.log(greet("Alice")); // Output: Hello, Alice!
 console.log(greetArrow("Bob")); // Output: Hello, Bob!
 console.log(greetShortArrow("Charlie")); // Output: Hello, Charlie!
 
-
-// Using ternary operator to loop 
+// Using ternary operator to loop
 const name = "simon";
 const age = 18;
 
@@ -25,3 +24,28 @@ const voter = () => {
   return age >= 18 ? "Allowed to vote" : "Not allowed to vote";
 };
 console.log(voter());
+
+//Getting good with Arrow functions
+const App = () => {
+  return "I love to program in java script and react";
+};
+console.log(App());
+// Normal Javascript function
+
+function AppLink() {
+  return "React is a javascript Library used building good looking Ui's ";
+}
+console.log(AppLink());
+
+//Traditional Way of writting javascript functions
+function handleClick() {
+  return "click here and learn more about javascript";
+}
+console.log(handleClick());
+
+//Arrow functions are more simple and concise way  of writting javascript
+const java = "React is a library while Angular is a framework";
+const arrowFunctions = () => {
+  return java;
+};
+console.log(arrowFunctions());
